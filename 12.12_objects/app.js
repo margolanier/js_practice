6293de597bd723b64f11d835c138b8ba43de65a6
Margo Lanier <lanier.margo@gmail.com>
2016-12-12 15:01:49 -0500

    created simple oregon trail game

---
+++ 12.12_objects/app.js
@@ -0,0 +1,79 @@
+/*	
+ *	Oregon Trail
+ */
+
+function makeTraveler(travelerName) {
+	return {
+		name: travelerName,
+		amountFood: Math.round( 20*Math.random() ),
+		isHealthy: true,
+	}
+}
+
+function makeWagon(wagonCapacity) {
+	return {
+		passengers: [],
+		capacity: wagonCapacity,
+	}
+}
+
+function hunt(traveler) {
+	let success = Math.round( Math.random() );
+	if (success === 1) {
+		traveler.amountFood += 100;
+	}
+}
+
+function eat(traveler) {
+	traveler.amountFood -= 20;
+	if (traveler.amountFood < 0) {
+		traveler.isHealthy = false;
+	}
+}
+
+function join(wagon, traveler) {
+	if (wagon.passengers < wagon.capacity) {
+		wagon.passengers.push(traveler);
+	}
+}
+
+function quarantine(wagon) {
+	let contaminated = false;
+	
+	for (let i=0; i<wagon.passengers.length; i++) {
+		if (wagon.passengers[i].isHealthy === false) {
+			contaminated = true;
+			return contaminated;
+		}
+	}
+	return contaminated;
+}
+
+function food(wagon) {
+	let totalFood = 0;
+	
+	for (let i=0; i<wagon.passengers.length; i++) {
+		total += wagon.passengers[i].amountFood;
+	}
+	return totalFood;
+}
+
+
+/*
+ * Play game
+ */
+let wagon = makeWagon(5);
+let traveler = makeTraveler('Henrietta');
+let traveler2 = makeTraveler('Juan');
+
+console.log(traveler2);
+
+hunt(traveler); // maybe get more food
+eat(traveler2);
+eat(traveler2); // juan is hungry
+join(wagon, traveler);
+join(wagon, traveler2);
+
+console.log(quarantine(wagon)); // print true if someone is sick, false otherwise
+console.log(food(wood)); // print juan's food + henrietta's food
+
