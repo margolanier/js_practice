420ca3e28c81d867fbfaee87681fa26f4d7d43cc
Margo Lanier <lanier.margo@gmail.com>
2016-12-15 11:12:40 -0500

    finished staying and fixed <form> page refresh bug

--- 12.14_jeopardy_api/app.js
+++ 12.14_jeopardy_api/app.js
@@ -2,20 +2,19 @@ let currentQ;
 let nextQ;
 let score = 0;
 
+let getNextQ = document.querySelector('#next');
+let displayScore = document.querySelector('.score');
+let response = document.querySelector('.response');
+
 function init() {
 	newQ();
 	
-	let displayScore = document.querySelector('.score');
 	displayScore.textContent = score;
 	
 	let submit = document.querySelector('#submit');
 	submit.addEventListener('click', checkAns);
 	
-	let buttonExists = document.querySelector('#next');
-	if (buttonExists != null) {
-		let getNextQ = document.querySelector('#next');
-		getNextQ.addEventListener('click', newQ);
-	}
+	getNextQ.addEventListener('click', newQ);
 }
 window.addEventListener('load', init);
 
@@ -44,21 +43,17 @@ function displayQ(Q) {
 	
 	let value = document.querySelector('.value');
 	value.textContent = Q.value;
+	
+	response.textContent = '';
+	getNextQ.style.display = "none";
 }
 
 function checkAns() {
-	let correctAns = currentQ.answer;
-	console.log('correct = ' + correctAns);
 	
+	let correctAns = currentQ.answer;
 	let textBox = document.querySelector('input');
 	let userAns = textBox.value;
 	textBox.value = '';
-	
-	console.log('user = ' + userAns);
-	
-	let response = document.querySelector('.response');
-	
-	let displayScore = document.querySelector('.score');
 	
 	if ( userAns.toLowerCase() === correctAns.toLowerCase() ) {
 		// User is correct, award points
@@ -70,11 +65,6 @@ function checkAns() {
 		response.textContent = 'Incorrect.  The correct answer is ' + correctAns;
 	}
 	
-	// After user answers question, add NEXT button
-	let nextQparent = document.querySelector('#next-question');
-	nextQ = document.createElement('button');
-	nextQ.setAttribute('id', 'next');
-	nextQ.textContent = 'Next Question';
-	nextQparent.appendChild(nextQ);
+	getNextQ.style.display = "block";
 }
 
