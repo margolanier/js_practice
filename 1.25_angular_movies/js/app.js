b6a81a9dceeaf4fec80aee5460dcd3db2f2ab1e4
Margo Lanier <lanier.margo@gmail.com>
2017-01-25 21:04:58 -0500

    added rating feature. styled the page.

--- 1.25_angular_movies/js/app.js
+++ 1.25_angular_movies/js/app.js
@@ -1,6 +1,6 @@
 const app = angular.module('MovieApp', []);
 
-app.controller('MovieController', function ($scope, MovieData) {
+app.controller('FormController', function ($scope, MovieData) {
 	// Get array of movies from the service
 	$scope.movies = MovieData.getMovies();
 	
@@ -13,6 +13,26 @@ app.controller('MovieController', function ($scope, MovieData) {
 		$scope.newDate = '';
 		$scope.newGenre = '';
 	};
+});
+
+app.controller('ListController', function ($scope, MovieData) {
+	$scope.movies = MovieData.getMovies();
+	
+	$scope.rate = function(item, rating) {
+		console.log(`you rated this ${rating}`);
+		item.rating = rating;
+		
+		/*$scope.movies[id].classList.remove('like');
+		$scope.movies[id].classList.remove('dislike');*/
+		
+		/*$scope.movies[id].classList.add('rating');*/
+	};
+	
+	$scope.ratingBtn = function (thought, target) {
+        console.log('You are clicking on rating');
+        
+        target.rating = thought;
+    } 
 	
 	$scope.remove = function(id) {
 		$scope.movies.splice(id, 1);
@@ -20,7 +40,26 @@ app.controller('MovieController', function ($scope, MovieData) {
 });
 
 app.factory('MovieData', function() {
-    let movies = [];
+    let movies = [
+		{
+			title: 'The Godfather',
+			releaseDate: 1972,
+			genre: 'Crime',
+			rating: null,
+		},
+		{
+			title: 'Star Wars',
+			releaseDate: 1977,
+			genre: 'Adventure',
+			rating: null,
+		},
+		{
+			title: 'E.T. the Extra-Terrestrial',
+			releaseDate: 1982,
+			genre: 'Sci-Fi',
+			rating: null,
+		},
+	];
 	
     return {
         addMovie: function(title, date, genre) {
@@ -28,6 +67,7 @@ app.factory('MovieData', function() {
 				title: title,
 				releaseDate: date,
 				genre: genre,
+				rating: null,
 			};
             movies.push(movie);
         },
